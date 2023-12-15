f60bc2f6373f39ca5e18cb08bb9a800cd6bfe273
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-12-15 18:32:07 +0200

    ✨ feat: LogIn page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import "./theme.scss";
 import SignUp from "./components/module_OR_PAGE/signUp";
+import LogIn from "./components/module_OR_PAGE/logIn";
 
 function App() {
   return (
     <>
-      <SignUp />
+      <LogIn />
     </>
   );
 }
