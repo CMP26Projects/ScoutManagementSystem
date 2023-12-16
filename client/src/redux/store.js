af877600a9b53f0587b16e429fdacf44f8f38d2f
akram <akramhany65@gmail.com>
2023-12-16 20:42:11 +0200

    🔥Include the user and scouts reducers into the store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
+import userReducer from "./slices/userSlice"
+import scoutsReducer from "./slices/scoutsSlice"
 
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        user: userReducer,
+        scouts: scoutsReducer,
+    },
 })
 
-export default store
+export default store;
