44842575b93bb20ab4b75be6d1acf8dc3cdd4497
amir-kedis <amir.kedis@gmail.com>
2023-12-16 12:11:48 +0200

    🔨 Rename Title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import Alert from "./components/common/Alerts";
-
 // Global Style (includes normalize.css)
 import "./assets/styles/global/global.scss";
 
@@ -8,14 +5,6 @@ function App() {
   return (
     <>
       <h1>HI there test normalize</h1>
-      <Alert
-        title="عنوان"
-        info="نص الرسالة"
-        buttontext="تمت المعاينة"
-        showRightBox={false}
-        Onclick={() => console.log("clicked")}
-        color="yellow"
-      />
     </>
   );
 }
