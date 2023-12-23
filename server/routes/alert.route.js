fd2ccf49b5185812d41d46bc37f6e07d986e2e9f
akram <akramhany65@gmail.com>
2023-12-24 01:04:19 +0200

    🔨fix: Make the routing of alerts match the one we are using

--- server/routes/alert.route.js
+++ server/routes/alert.route.js
@@ -1,9 +1,11 @@
-const alertRouter = require("express").Router();
-const alertController = require("../controllers/alert.controller");
+import {Router} from "express"
+import alertController from "../controllers/alert.controller.js"
+
+const alertRouter = Router();
 
 alertRouter.get("/", alertController.getAllAlerts);
 alertRouter.get("/:id", alertController.getAlert);
 alertRouter.post("/post", alertController.CreateAlert);
 alertRouter.delete("/:id", alertController.DeleteAlert);
 
-module.exports = alertRouter;
+export default alertRouter;
--- server/routes/captain.route.js
+++ server/routes/captain.route.js
@@ -1,11 +1,8 @@
 import { Router } from "express"
-import authMiddleware from "../middlewares/auth.middleware.js";
 import captainController from "../controllers/captain.controller.js";
 
 
 const captainRouter = Router();
-
-// Check that the user is authorized
 
 
 captainRouter.get('/allCaptains/info', captainController.allCaptainsInfo)
