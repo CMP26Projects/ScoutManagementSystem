e6f77e7b4f4b1b225605c3814e8920b04579332c
amir-kedis <amir.kedis@gmail.com>
2023-12-23 20:15:34 +0200

    ✨ feat: signin new login

--- client/src/components/login/logIn.jsx
+++ client/src/components/login/logIn.jsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
 import Button from "../common/Button";
+import { Link } from "react-router-dom";
+import TextInput from "../common/Inputs";
 import "./logIn.scss";
 
 export default function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <div className="login">
-      <div className="hero">
+      <form onSubmit={submitHandler} className="hero">
         <h2>تسجيل الدخول</h2>
         <div className="card">
-          <label className="input-field">
-            الحساب
-            <input type="email" />
-          </label>
-          <label className="input-field">
-            الرمز السري
-            <input type="password" />
-          </label>
-          <Button className="Button--medium Button--success" linkTo="/signUp">
+          <TextInput
+            label="البريد الالكتروني"
+            type="email"
+            name="email"
+            value={email}
+            placeholder="أكتب بريدك الالكتروني"
+            onChange={(e) => setEmail(e.target.value)}
+            required={true}
+          />
+          <TextInput
+            label="الرمز السري"
+            type="password"
+            name="password"
+            value={password}
+            placeholder="أكتب الرمز السري"
+            onChange={(e) => setPassword(e.target.value)}
+            required={true}
+          />
+          <Button type="submit" className="Button--medium Button--success">
             تسجيل الدخول
           </Button>
         </div>
-        <div className="small no-account">ليس لديك حساب؟</div>
-      </div>
+        <Link to="/signUp" className="small no-account">
+          ليس لديك حساب؟
+        </Link>
+      </form>
     </div>
   );
 }
