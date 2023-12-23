822786c7c2a0406bcd2889ec4d0ed6f9da74a4e9
akram <akramhany65@gmail.com>
2023-12-24 00:12:13 +0200

    🔨 fix: move the authMiddleware to api.route

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import authRouter from './auth.route.js'
 import captainRouter from './captain.route.js'
+import authMiddleware from '../middlewares/auth.middleware.js'
 
 const apiRouter = Router()
 
 apiRouter.use('/auth', authRouter)
-apiRouter.use('/captain', captainRouter)
+apiRouter.use('/captain', authMiddleware, captainRouter)
 
 export default apiRouter
--- server/routes/captain.route.js
+++ server/routes/captain.route.js
@@ -6,7 +6,7 @@ import captainController from "../controllers/captain.controller.js";
 const captainRouter = Router();
 
 // Check that the user is authorized
-//captainRouter.use('/', authMiddleware);
+
 
 captainRouter.get('/allCaptains/info', captainController.allCaptainsInfo)
 captainRouter.get('/allCaptains/count', captainController.allCaptainsCount)
