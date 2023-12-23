b2e4aae17f2dee79331fcfc8dea64d61f0c5d3e6
amir-kedis <amir.kedis@gmail.com>
2023-12-23 20:24:39 +0200

    ⚛️ chore: set the redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userReducer from "./slices/userSlice"
-import scoutsReducer from "./slices/scoutsSlice"
-
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
+import scoutsReducer from "./slices/scoutsSlice";
 
 const store = configureStore({
-    reducer: {
-        user: userReducer,
-        scouts: scoutsReducer,
-    },
-})
+  reducer: {
+    user: userReducer,
+    scouts: scoutsReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
+});
 
 export default store;
