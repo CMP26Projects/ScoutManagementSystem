dd52f4c53b2289f5578c6df863a9f437c463b075
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-12-25 01:35:29 +0200

    ✨ feat: get budget

--- server/controllers/finance.controller.js
+++ server/controllers/finance.controller.js
@@ -6,10 +6,28 @@ const financeController = {
     // @access  Private
     getBudget: async (req, res) => {
         try {
-            // get subsciptions
-            return res.status(200)
-        } catch (err) {
-            return res.status(500).json({ message: err.message })
+            // get income
+            let result = await db.query(
+                `SELECT COALESCE(SUM(value), 0) AS sum FROM "FinanceItem" WHERE "type" = 'income'`
+            )
+            const income = result.rows[0].sum
+
+            // get expense
+            result = await db.query(
+                `SELECT COALESCE(SUM(value), 0) AS sum FROM "FinanceItem" WHERE "type" = 'expense'`
+            )
+            const expense = result.rows[0].sum
+
+            const budget = income - expense
+            res.status(200).json({
+                message: 'Get budget successfully',
+                body: budget,
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                error: 'An error occurred while getting the budget',
+            })
         }
     },
 }
