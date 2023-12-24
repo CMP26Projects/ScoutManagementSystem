25d94c928c2dd058d5932d652313e45f7754cb7a
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-12-25 01:37:26 +0200

    🎨 chore: add middleware to get current stuff

--- server/routes/term.route.js
+++ server/routes/term.route.js
@@ -1,18 +1,36 @@
 import { Router } from 'express'
 import termController from '../controllers/term.controller.js'
 import checkRankMiddleware from '../middlewares/checkRank.middleware.js'
+import {
+    getCurrentTermMiddleware,
+    getCurrentWeekMiddleware,
+} from '../middlewares/current.middleware.js'
 const termRouter = Router()
 
 // Term routes
-termRouter.get('/', termController.getTerm)
-termRouter.post('/', checkRankMiddleware('general'), termController.addTerm)
+termRouter.get('/', getCurrentTermMiddleware, termController.getTerm)
+termRouter.post(
+    '/',
+    checkRankMiddleware('general'),
+    getCurrentTermMiddleware,
+    termController.addTerm
+)
 termRouter.patch('/', checkRankMiddleware('general'), termController.updateTerm)
 
 // Week routes
-termRouter.get('/week', termController.getWeek)
-termRouter.patch('/week', checkRankMiddleware('general'), termController.cancelWeek)
+termRouter.get('/week', getCurrentWeekMiddleware, termController.getWeek)
+termRouter.patch(
+    '/week',
+    checkRankMiddleware('general'),
+    getCurrentWeekMiddleware,
+    termController.cancelWeek
+)
 
 // Other routes
-termRouter.get('/remaining', termController.getRemainingWeeks)
+termRouter.get(
+    '/remaining',
+    getCurrentTermMiddleware,
+    termController.getRemainingWeeks
+)
 
 export default termRouter
