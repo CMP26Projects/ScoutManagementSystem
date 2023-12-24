43ad800ff0a4e2e11d6bc6a93d80c110e951d44f
amir-kedis <amir.kedis@gmail.com>
2023-12-24 10:48:36 +0200

    ⚛️ feat: add to dashboard Page title

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,5 +1,24 @@
+import PageTitle from "../common/PageTitle";
 import "./Dashboard.scss";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { userInfo } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/logIn");
+    }
+  }, [navigate, userInfo]);
+
+  const titleMsg = `مرحباً يا كاتبن ${userInfo?.firstName} ${userInfo?.middleName}`;
+
+  return (
+    <div className="dashboard">
+      <PageTitle title={titleMsg} />
+    </div>
+  );
 }
