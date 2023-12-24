7b5f23fb1489e3e769ab1c1ff9771cea8b96ea69
akram <akramhany65@gmail.com>
2023-12-24 13:01:21 +0200

    ✨feat: Implement getting all scouts info

--- server/controllers/scout.controller.js
+++ server/controllers/scout.controller.js
@@ -20,7 +20,27 @@ const scoutController = {
                 error
             })
         }
-    }
+    },
+    allScoutsInfo: async (req, res) => {
+        try {
+            const result = await db.query(`
+            SELECT *
+            FROM "Scout"
+            `)
+
+            res.status(200).json({
+                message: "Successful retrieval",
+                body: result,
+            })
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'An error occured while retrieving data',
+                error
+            })
+        }
+    },
 }
 
 export default scoutController;
--- server/routes/scout.route.js
+++ server/routes/scout.route.js
@@ -5,5 +5,6 @@ import scoutController from "../controllers/scout.controller.js";
 const scoutRouter = Router();
 
 scoutRouter.get('/allScouts/count', scoutController.allScoutsCount)
+scoutRouter.get('/allScouts/info', scoutController.allScoutsInfo)
 
 export default scoutRouter
