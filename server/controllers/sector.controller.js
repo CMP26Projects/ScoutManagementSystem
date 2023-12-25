b5a9a9beda427c6ba354d201ad0d71d79be8a79f
akram <akramhany65@gmail.com>
2023-12-25 23:27:15 +0200

    🔥 feat: Implement getting a sector by its name.

--- server/controllers/sector.controller.js
+++ server/controllers/sector.controller.js
@@ -1,6 +1,9 @@
 import db from '../database/db.js'
 
 const sectorController = {
+    // @desc    Get all sectors (info and count)
+    // @route   GET /api/sector/all
+    // @access  Private
     getAllSectors: async (req, res) => {
         try {
             const result = await db.query(`
@@ -20,7 +23,41 @@ const sectorController = {
                 body: error,
             })
         }
-    }
+    },
+
+    // @desc    Get sector by id (baseName and suffixName send as params)
+    // @route   GET /api/sector/:baseName/:suffixName
+    // @access  Private
+    getSector: async (req, res) => {
+        try {
+            const { baseName, suffixName } = req.params
+
+            const result = await db.query(`
+                SELECT *
+                FROM "Sector"
+                WHERE "baseName" = $1 AND "suffixName" = $2;
+            `,
+            [baseName, suffixName]);
+
+            if (result.rowCount === 0) {
+                return res.status(404).json({
+                    error: "No sector found with this name"
+                })
+            }
+
+            res.status(200).json({
+                message: "Successful retrieval",
+                body: result.rows,
+            })
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                error: 'An error occured while retrieving the captains info',
+                body: error,
+            })
+        }
+    },
 }
 
 export default sectorController;
--- server/routes/sector.route.js
+++ server/routes/sector.route.js
@@ -4,5 +4,7 @@ import sectorController from "../controllers/sector.controller.js"
 const sectorRouter = Router();
 
 sectorRouter.get('/all', sectorController.getAllSectors)
+sectorRouter.get('/:baseName/:suffixName', sectorController.getSector)
+
 
 export default sectorRouter;
