840ee389d7ef45821adaf8a5e5895485968231df
akram <akramhany65@gmail.com>
2023-12-27 20:12:51 +0200

    🚑 fix: Change the path params to query params

--- server/routes/attendance.route.js
+++ server/routes/attendance.route.js
@@ -5,7 +5,7 @@ const attendanceRouter = Router()
 
 // Insert a new attendance for a scout
 attendanceRouter.post('/', attendanceController.insertAttendance)
-attendanceRouter.get('/sector/:baseName/:suffixName/:weekNumber/:termNumber', attendanceController.getSectorAttendance)
+attendanceRouter.get('/sector/all', attendanceController.getSectorAttendance)
 attendanceRouter.get('/:scoutId/:weekNumber/:termNumber', attendanceController.getScoutAttendance)
 
 export default attendanceRouter;
