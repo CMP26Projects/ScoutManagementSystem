9a65e5c82f85bbe00a487f773fb971e583a799eb
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-12-29 01:27:29 +0200

    ✨ feat: add notification box in dashboard

--- client/src/components/common/NotificationBox.jsx
+++ client/src/components/common/NotificationBox.jsx
@@ -1,9 +1,24 @@
-import "../../assets/styles/components/NotificationBox.scss";
+import '../../assets/styles/components/NotificationBox.scss'
+import { useGetAllAlertsQuery } from '../../redux/slices/alertApiSlice'
 
 export default function NotificationBox() {
-  return (
-    <div style={{ textAlign: "center", marginBlock: "1rem" }}>
-      سيتم العمل على هذا القسم قريباً
-    </div>
-  );
+    const { data: alerts, isFetching: isFetchingAlerts } =
+        useGetAllAlertsQuery();
+    
+    return (
+        <div className="notification-box">
+          {isFetchingAlerts ? "جاري التحميل" : alerts?.body?.length === 0 ? "لا يوجد إشعارات" : alerts?.body?.map((alert) => {
+            return (
+              <div className="notification-box__alert">
+                <div className="notification-box__alert__title">
+                  {alert.title}
+                </div>
+                <div className="notification-box__alert__body">
+                  {alert.body}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+    )
 }
---
+++ client/src/redux/slices/alertApiSlice.js
@@ -0,0 +1,65 @@
+import { apiSlice } from './apiSlice'
+
+const ALERT_URL = '/api/alert'
+
+export const alertApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        GetAllAlerts: builder.query({
+            query: (status, contentType) => ({
+                url: `${ALERT_URL}/all`,
+                method: 'GET',
+                params: { status: status, contentType: contentType },
+            }),
+            providesTags: ['alert'],
+        }),
+        GetAlert: builder.query({
+            query: (id) => ({
+                url: `${ALERT_URL}/${id}`,
+                method: 'GET',
+            }),
+            providesTags: ['alert'],
+        }),
+        SendAlert: builder.mutation({
+            query: (id, sectorBaseName, sectorSuffixName) => ({
+                url: `${ALERT_URL}/${id}`,
+                method: 'POST',
+                body: {
+                    sectorBaseName: sectorBaseName,
+                    sectorSuffixName: sectorSuffixName,
+                },
+            }),
+            invalidatesTags: ['alert'],
+        }),
+        CreateAlert: builder.mutation({
+            query: (alert) => ({
+                url: `${ALERT_URL}`,
+                method: 'POST',
+                body: alert,
+            }),
+            invalidatesTags: ['alert'],
+        }),
+        UpdateStatus: builder.mutation({
+            query: (id) => ({
+                url: `${ALERT_URL}/${id}`,
+                method: 'PATCH',
+            }),
+            invalidatesTags: ['alert'],
+        }),
+        DeleteAlert: builder.mutation({
+            query: (id) => ({
+                url: `${ALERT_URL}/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['alert'],
+        }),
+    }),
+})
+
+export const {
+    useGetAllAlertsQuery,
+    useGetAlertQuery,
+    useSendAlertMutation,
+    useCreateAlertMutation,
+    useUpdateStatusMutation,
+    useDeleteAlertMutation,
+} = alertApi
