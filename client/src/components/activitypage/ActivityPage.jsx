fbe23f6b8d11b073b6c744373bd0aa3283c656e5
Abdelruhman Sami <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2023-12-29 04:18:06 +0200

    activity page front end done

--- client/src/components/activitypage/ActivityPage.jsx
+++ client/src/components/activitypage/ActivityPage.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import PageTitle from "../common/PageTitle";
 import ActivityCard from "../common/activityCard";
+import Button from "../common/Button";
+
+import "./ActivityPage.scss";
+
 
 const ActivityPage = () => {
   return (
-    <>
+    <div className="activity-page">
       <PageTitle title="الأنشطة" />
-      <Button className="Button--medium --success">تعديل</Button>
+      <Button className="Button--medium Button--success add-button">إضافة</Button>
       <div className="activity-section">
         <ActivityCard title="تخييم" place="محمية وادي دجلة" time="يوم الجمعة" />
         <ActivityCard title="تخييم" place="محمية وادي دجلة" time="يوم الجمعة" />
         <ActivityCard title="تخييم" place="محمية وادي دجلة" time="يوم الجمعة" />
         <ActivityCard title="تخييم" place="محمية وادي دجلة" time="يوم الجمعة" />
       </div>
-    </>
+    </div>
   );
 };
 
