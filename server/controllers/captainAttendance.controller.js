c63f842cd2c06e7a5c3211fb91711048da08853e
akram <akramhany65@gmail.com>
2023-12-29 12:01:26 +0200

    ✨ feat: Implement getting the attendance of a unit

--- server/controllers/captainAttendance.controller.js
+++ server/controllers/captainAttendance.controller.js
@@ -130,6 +130,37 @@ const captainAttendanceController = {
                 body: error,
             })
         }
+    },
+    getUnitAttendance: async (req, res) => {
+        try {
+            const { unitCaptainId, weekNumber, termNumber } = req.query
+
+            const result = await db.query(`
+                SELECT "Captain".*, "CaptainAttendance".* FROM "Captain" LEFT JOIN "CaptainAttendance" ON "Captain"."captainId" = "CaptainAttendance"."captainId"
+                AND "CaptainAttendance"."weekNumber" = $2 AND "CaptainAttendance"."termNumber" = $3
+                INNER JOIN "Sector" ON "Sector"."baseName" = "Captain"."rSectorBaseName" AND "Sector"."suffixName" = "Captain"."rSectorSuffixName"
+                WHERE "Sector"."unitCaptainId" = $1;
+            `,
+            [unitCaptainId, weekNumber, termNumber])
+
+            if (result.rowCount === 0) {
+                return res.status(404).json({
+                    error: "No data exists for the provided info"
+                })
+            }
+
+            res.status(200).json({
+                message: "Successful retrieval",
+                body: result.rows,
+                count: result.rowCount
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                error: 'An error occured while retrieving unit Attendance',
+                body: error,
+            })
+        }
     }
 }
 
