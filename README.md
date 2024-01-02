a8e8b2311da7da7ca433831ada8a7529b47b24f6
Abdelruhman Sami <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-01-03 00:08:05 +0200

    🔥 feat: Updating README

--- README.md
+++ README.md
@@ -1,8 +1,83 @@
-# ScoutManagementSystem
-## About the repo
-This repo includes a scout management system project implemented using PERN stack. This project is a 2nd year FE-CU project for practicing on the fundemental rules of database system management.
+# Scout Management System
+
+![Logo](READMEImages/logo.png)
+
+## About the Project
+
+Scout management system is a website implemented using **PERN stack**. This project is a 2nd year CUFE project for practicing on the fundemental rules of database system management.
+
+![Landing Page](READMEImages/Landing%20Page%20Cropped.png)
+
+## Our website is now on air🔥
+
+We deployed our website, although, it is still in progress so you might find some missing functoinalities.
+
+To login as a general captain use:
+
+> email: general@gmail.com
+> pass: 1234
+
+🔥[Website Link](https://scouts-managment-system-development.onrender.com/)🔥
+
+## Why do you have to use this website?
+
+This website is based on a well structured database and a user friendly app, which helps in providing the best managerial experience to the scout captains. It helps them in:
+
+### Scout management
+
+- Getting Scouts' absence and statistics over a certain interval
+- Sending automatic notifications in case of exceeding the limit of absence
+- Adding/removing them from/to any sector
+
+### Sectors management
+
+- Adding/removing new sectors from/to the system
+- Managing leading captains
+- Managing Absence for each sector's leading captains
+
+### Activities and sessions
+
+- Saving each activity including it's type, place and time
+- Grading scouts on the activities they joined and showing stats
+
+### Finance management
+
+- Adding weekly subscriptions
+- Adding incomes and expenses to the system
+- Getting updated value of the total balance and each transaction has been done
+
+### Stats
+
+- Viewing attendance stats for scouts and captains
+- Providing graphs for better analysis
+- Getting reports on the current system status
+
+## Pre-implementation phase
+
+### Database design
+
+ER-design and schema designs for the database
+
+![ER](READMEImages/scouts-managment-system-er-no-attr.drawio.png)
+![Schema](READMEImages/Schema.jpg)
+
+### User Stories
+
+[User Stories](https://drive.google.com/file/d/1RwYU0OpM7EVVqLCTXDNxmlU7WgaKOdvz/view?usp=sharing)
+
+### Figma Design
+
+We have designed a complete UX design for all system pages: about 40 pages  
+![Figma Design](READMEImages/FigmaDesign.png)
+[View full figma design](https://www.figma.com/file/Yj8KCaGVDlDprgecgPdMwq/Scouts-Management-System?type=design&node-id=0%3A1&mode=design&t=rPMKLEZWaE6dBebp-1)
+
+## How to run?
+
+run the following commands in the terminal
+![Commands](READMEImages/carbon.png)
+
 ## Contributers
-[AbdelruhmanSamy](https://github.com/AbdelruhmanSamy/)
-[amir-kedis](https://github.com/amir-kedis/)
-[akramhany](https://github.com/akramhany/)
-[AhmedHamed3699](https://github.com/AhmedHamed3699)
+
+| ![Abdelruhman](https://avatars.githubusercontent.com/u/121282837?v=4) | ![Amir](https://avatars.githubusercontent.com/u/88613195?v=4) | ![Akram](https://avatars.githubusercontent.com/u/109467185?v=4) | ![Ahmed](https://avatars.githubusercontent.com/u/104217693?v=4) |
+| --------------------------------------------------------------------- | ------------------------------------------------------------- | --------------------------------------------------------------- | --------------------------------------------------------------- |
+| [AbdelruhmanSamy](https://github.com/AbdelruhmanSamy/)                | [amir-kedis](https://github.com/amir-kedis/)                  | [akramhany](https://github.com/akramhany/)                      | [AhmedHamed3699](https://github.com/AhmedHamed3699)             |
--- server/controllers/term.controller.js
+++ server/controllers/term.controller.js
@@ -1,237 +1,236 @@
-import db from '../database/db.js'
+import db from "../database/db.js";
 
 const termController = {
-    // @desc    Get a term
-    // @route   GET /api/term/
-    // @access  Private
-    getTerm: async (req, res) => {
-        if (req.currentTerm.termNumber === 0) {
-            return res.status(400).json({
-                error: 'There is no terms in the system',
-            })
-        }
-        res.status(200).json({
-            message: 'Current term found successfully',
-            body: req.currentTerm,
-        })
-    },
+  // @desc    Get a term
+  // @route   GET /api/term/
+  // @access  Private
+  getTerm: async (req, res) => {
+    if (req.currentTerm.termNumber === 0) {
+      return res.status(400).json({
+        error: "There is no terms in the system",
+      });
+    }
+    res.status(200).json({
+      message: "Current term found successfully",
+      body: req.currentTerm,
+    });
+  },
 
-    // @desc    Add a term
-    // @route   POST /api/term/
-    // @access  Private
-    addTerm: async (req, res) => {
-        try {
-            const { termName, startDate, endDate } = req.body
+  // @desc    Add a term
+  // @route   POST /api/term/
+  // @access  Private
+  addTerm: async (req, res) => {
+    try {
+      const { termName, startDate, endDate } = req.body;
 
-            const currentDate = new Date()
-            const startDateObj = new Date(startDate)
-            const endDateObj = new Date(endDate)
-            if (startDateObj >= endDateObj || endDateObj < currentDate) {
-                return res.status(400).json({
-                    error: 'Invalid dates',
-                })
-            }
-            if (
-                req.currentTerm.termNumber &&
-                req.currentTerm.endDate >= startDateObj
-            ) {
-                return res.status(400).json({
-                    error: 'Invalid start date: Overlapping terms',
-                })
-            }
+      const currentDate = new Date();
+      const startDateObj = new Date(startDate);
+      const endDateObj = new Date(endDate);
+      if (startDateObj >= endDateObj || endDateObj < currentDate) {
+        return res.status(400).json({
+          error: "Invalid dates",
+        });
+      }
+      if (
+        req.currentTerm.termNumber &&
+        req.currentTerm.endDate >= startDateObj
+      ) {
+        return res.status(400).json({
+          error: "Invalid start date: Overlapping terms",
+        });
+      }
 
-            const termNumber = req.currentTerm.termNumber + 1
-            const result = await db.query(
-                `INSERT INTO "Term" VALUES ($1, $2, $3, $4)
+      const termNumber = req.currentTerm.termNumber + 1;
+      const result = await db.query(
+        `INSERT INTO "Term" VALUES ($1, $2, $3, $4)
                 RETURNING *;`,
-                [termNumber, termName, startDate, endDate]
-            )
+        [termNumber, termName, startDate, endDate]
+      );
 
-            const newTerm = result.rows[0]
+      const newTerm = result.rows[0];
 
-            res.status(201).json({
-                message: 'Term added successfully',
-                body: newTerm,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'An error occurred while adding the term',
-            })
-        }
-    },
+      res.status(201).json({
+        message: "Term added successfully",
+        body: newTerm,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "An error occurred while adding the term",
+      });
+    }
+  },
 
-    // @desc    Update a term
-    // @route   PATCH /api/term/
-    // @access  Private
-    updateTerm: async (req, res) => {
-        try {
-            const { termName, startDate, endDate } = req.body
+  // @desc    Update a term
+  // @route   PATCH /api/term/
+  // @access  Private
+  updateTerm: async (req, res) => {
+    try {
+      const { termName, startDate, endDate } = req.body;
 
-            const currentDate = new Date()
-            const startDateObj = new Date(startDate)
-            const endDateObj = new Date(endDate)
-            if (startDateObj >= endDateObj || endDateObj < currentDate) {
-                return res.status(400).json({
-                    error: 'Invalid dates',
-                })
-            }
+      const currentDate = new Date();
+      const startDateObj = new Date(startDate);
+      const endDateObj = new Date(endDate);
+      if (startDateObj >= endDateObj || endDateObj < currentDate) {
+        return res.status(400).json({
+          error: "Invalid dates",
+        });
+      }
 
-            let result = await db.query(
-                `SELECT * FROM "Term"
+      let result = await db.query(
+        `SELECT * FROM "Term"
                 ORDER BY "termNumber" DESC
                 LIMIT 1 OFFSET 1;`
-            )
-            if (!result.rowCount) {
-                req.previousTerm = {
-                    termNumber: 0,
-                }
-            } else req.previousTerm = result.rows[0]
+      );
+      if (!result.rowCount) {
+        req.previousTerm = {
+          termNumber: 0,
+        };
+      } else req.previousTerm = result.rows[0];
 
-            if (
-                req.previousTerm.termNumber &&
-                req.previousTerm.endDate >= startDateObj
-            ) {
-                return res.status(400).json({
-                    error: 'Invalid start date: Overlapping terms',
-                })
-            }
+      if (
+        req.previousTerm.termNumber &&
+        req.previousTerm.endDate >= startDateObj
+      ) {
+        return res.status(400).json({
+          error: "Invalid start date: Overlapping terms",
+        });
+      }
 
-            result = await db.query(
-                `UPDATE "Term" SET "termName" = $1, "startDate" = $2, "endDate" = $3
+      result = await db.query(
+        `UPDATE "Term" SET "termName" = $1, "startDate" = $2, "endDate" = $3
                 WHERE "termNumber" IN
                 (SELECT COALESCE(MAX("termNumber"), 0) FROM "Term")
                 RETURNING *;`,
-                [termName, startDate, endDate]
-            )
-            if (!result.rows.length) {
-                return res.status(400).json({
-                    error: 'There is no terms in the system',
-                })
-            }
+        [termName, startDate, endDate]
+      );
+      if (!result.rows.length) {
+        return res.status(400).json({
+          error: "There is no terms in the system",
+        });
+      }
 
-            const updatedTerm = result.rows[0]
+      const updatedTerm = result.rows[0];
 
-            res.status(200).json({
-                message: 'Term updated successfully',
-                body: updatedTerm,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'An error occurred while updating the term',
-            })
-        }
-    },
+      res.status(200).json({
+        message: "Term updated successfully",
+        body: updatedTerm,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "An error occurred while updating the term",
+      });
+    }
+  },
 
-    // @desc    Get a week
-    // @route   GET /api/term/week
-    // @access  Private
-    getCurrentWeek: async (req, res) => {
-        try {
-            if (req.currentWeek.weekNumber === 0) {
-                return res.status(400).json({
-                    error: 'There is no weeks or terms in the system',
-                })
-            }
-            res.status(200).json({
-                message: 'Current week found successfully',
-                body: req.currentWeek,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'An error occurred while getting the week',
-            })
-        }
-    },
+  // @desc    Get a week
+  // @route   GET /api/term/week
+  // @access  Private
+  getCurrentWeek: async (req, res) => {
+    try {
+      if (req.currentWeek.weekNumber === 0) {
+        return res.status(400).json({
+          error: "There is no weeks or terms in the system",
+        });
+      }
+      res.status(200).json({
+        message: "Current week found successfully",
+        body: req.currentWeek,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "An error occurred while getting the week",
+      });
+    }
+  },
 
-    // @desc    Get all weeks in term
-    // @route   GET /api/term/week/all
-    // @access  Private
-    getAllWeeks: async (req, res) => {
-        try {
-            const result = await db.query(
-                `SELECT * FROM "Week"
+  // @desc    Get all weeks in term
+  // @route   GET /api/term/week/all
+  // @access  Private
+  getAllWeeks: async (req, res) => {
+    try {
+      const result = await db.query(
+        `SELECT * FROM "Week"
                 WHERE "termNumber" = $1;`,
-                [req.currentTerm.termNumber]
-            )
+        [req.currentTerm.termNumber]
+      );
 
-            res.status(200).json({
-                message: 'all weeks in current term found successfully',
-                body: result.rows,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'An error occurred while getting all weeks',
-            })
-        }
-    },
+      res.status(200).json({
+        message: "all weeks in current term found successfully",
+        body: result.rows,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "An error occurred while getting all weeks",
+      });
+    }
+  },
 
-    // @desc    Cancel a week
-    // @route   PATCH /api/term/week
-    // @access  Private
-    cancelWeek: async (req, res) => {
-        try {
-            if (req.currentWeek.weekNumber === 0) {
-                return res.status(400).json({
-                    error: 'There is no weeks or terms in the system to be cancelled',
-                })
-            }
+  // @desc    Cancel a week
+  // @route   PATCH /api/term/week
+  // @access  Private
+  cancelWeek: async (req, res) => {
+    try {
+      if (req.currentWeek.weekNumber === 0) {
+        return res.status(400).json({
+          error: "There is no weeks or terms in the system to be cancelled",
+        });
+      }
 
-            const result = await db.query(
-                `UPDATE "Week" SET "cancelled" = true
+      const result = await db.query(
+        `UPDATE "Week" SET "cancelled" = true
                 WHERE "termNumber" = $1 AND "weekNumber" = $2
                 RETURNING *;`,
-                [req.currentWeek.termNumber, req.currentWeek.weekNumber]
-            )
+        [req.currentWeek.termNumber, req.currentWeek.weekNumber]
+      );
 
-            const updatedWeek = result.rows[0]
+      const updatedWeek = result.rows[0];
 
-            res.status(200).json({
-                message: 'Week cancelled successfully',
-                body: updatedWeek,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'An error occurred while cancelling the week',
-            })
-        }
-    },
+      res.status(200).json({
+        message: "Week cancelled successfully",
+        body: updatedWeek,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "An error occurred while cancelling the week",
+      });
+    }
+  },
 
-    // @desc    Get remaining weeks
-    // @route   GET /api/term/remaining
-    // @access  Private
-    getRemainingWeeks: async (req, res) => {
-        try {
-            const currentDate = new Date()
-            if (
-                req.currentTerm.termNumber === 0 ||
-                req.currentTerm.endDate < currentDate
-            ) {
-                return res.status(400).json({
-                    error: 'There is no running term in the system',
-                })
-            }
+  // @desc    Get remaining weeks
+  // @route   GET /api/term/remaining
+  // @access  Private
+  getRemainingWeeks: async (req, res) => {
+    try {
+      const currentDate = new Date();
+      if (
+        req.currentTerm.termNumber === 0 ||
+        req.currentTerm.endDate < currentDate
+      ) {
+        return res.status(400).json({
+          error: "There is no running term in the system",
+        });
+      }
 
-            const remainingWeeks = Math.ceil(
-                (req.currentTerm.endDate - currentDate) /
-                    (1000 * 60 * 60 * 24 * 7)
-            )
+      const remainingWeeks = Math.ceil(
+        (req.currentTerm.endDate - currentDate) / (1000 * 60 * 60 * 24 * 7)
+      );
 
-            res.status(200).json({
-                message: 'Remaining weeks found successfully',
-                body: remainingWeeks,
-            })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'An error occurred while getting the remaining weeks',
-            })
-        }
-    },
-}
+      res.status(200).json({
+        message: "Remaining weeks found successfully",
+        body: remainingWeeks,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "An error occurred while getting the remaining weeks",
+      });
+    }
+  },
+};
 
-export default termController
+export default termController;
